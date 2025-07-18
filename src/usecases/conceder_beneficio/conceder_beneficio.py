5a025b3956128d868a7f151245236cb32bf9d8d9
emanueldias01 <emanueldias01dev@gmail.com>
2025-07-18 13:09:29 -0300

    conceder beneficio done

---
+++ src/usecases/conceder_beneficio/conceder_beneficio.py
@@ -0,0 +1,20 @@
+from db.connection import get_cursor_and_connection
+from datetime import date
+
+def conceder_beneficio_a_familia(idB : int, idF : int):
+    cursor, conn = get_cursor_and_connection()
+    try:
+        cursor.execute("""
+            INSERT INTO tem(idFamilia, idBeneficio, dataInicio)
+            values(%s, %s, %s)
+        """,
+        (idB, idF, date.today))
+
+        conn.commit()
+
+    except Exception as e:
+        conn.rollback()
+        print(f'Erro ao conceder beneficio: {e}')
+    finally:
+        cursor.close()
+        conn.close()
