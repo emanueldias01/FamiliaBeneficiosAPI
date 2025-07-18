26f8873bea854ee903de44de304a97f58ee80c0a
KpSantiago <santiago.kauajose12@gmail.com>
2025-07-18 19:39:13 -0300

    fix: adicionar call para o metodo today

--- src/usecases/conceder_beneficio/conceder_beneficio.py
+++ src/usecases/conceder_beneficio/conceder_beneficio.py
@@ -1,14 +1,14 @@
 from db.connection import get_cursor_and_connection
 from datetime import date
-
-def conceder_beneficio_a_familia(idB : int, idF : int):
+  
+def conceder_beneficio_a_familia  (idB : int, idF : int):
     cursor, conn = get_cursor_and_connection()
     try:
         cursor.execute("""
             INSERT INTO tem(idFamilia, idBeneficio, dataInicio)
             values(%s, %s, %s)
         """,
-        (idB, idF, date.today))
+        (idB, idF, date.today()))
 
         conn.commit()
 
