a04ca7f6e4c6b95aef10538a09a4b9830a7d71a5
emanueldias01 <emanueldias01dev@gmail.com>
2025-07-18 11:10:48 -0300

    vizualizar familias cadastradas

---
+++ src/usecases/vizualizar_familias/listar_familias.py
@@ -0,0 +1,27 @@
+from db.connection import get_cursor_and_connection
+from models.familia.familia import Familia
+
+def lista_familias_cadastradas():
+    cursor, conn = get_cursor_and_connection()
+    cursor.execute('SELECT * FROM familia')
+    result = cursor.fetchall()
+
+    familias = []
+
+    for row in result:
+        familia = Familia(
+            idFamilia=row[0],
+            nome=row[1],
+            rua=row[2],
+            numero=row[3],
+            bairro=row[4],
+            cidade=row[5],
+            telefone=row[6],
+            renda=row[7],
+            numeroMembros=row[8]
+        )
+        familias.append(familia)
+
+    cursor.close()
+    conn.close()
+    return familias
